d7adf12393d90f483b918e0255e23e41a2b81e42
wolverineo250kr <wolverineo250kr@ya.ru>
2023-05-13 01:35:51 +0500

    Signed-off-by: wolverineo250kr <wolverineo250kr@ya.ru>

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -10,3 +10,18 @@ document.getElementById('btn_clr').addEventListener('click', function () {
     inputWindow.value = '';
 })
 
+
+document.querySelectorAll('.number').forEach(occurence => {
+    occurence.addEventListener('click', (e) => {
+        inputWindow.value = inputWindow.value + e.target.innerText;
+    });
+});
+
+
+document.querySelector('#btn_equals').addEventListener('click', function () {
+    inputWindow.value = evil(inputWindow.value);
+})
+
+function evil(fn) {
+    return new Function('return ' + fn)();
+}
